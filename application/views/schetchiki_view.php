09fcd8f2b25c6930f80bf2ecafe9d82bddc19be2
kostt <felix-mazur@mail.ru>
2016-04-09 09:47:03 +0300

    menu_2

--- application/views/schetchiki_view.php
+++ application/views/schetchiki_view.php
@@ -1,18 +1,18 @@
 
 <div class="dop_menu">
-<a style="float:left;" class="text_ramka" href="schetchiki"><div class="ramka">Счетчики воды. Установка и замена<img class="swf" src="/images/schetchiki.gif" width="115" height="60"></div></a>	
-<a style="float:left;" class="text_ramka" href="radiatory"><div class="ramka">Замена радиаторов отопления<img class="swf" src="/images/radiator.gif" width="80" height="60"></div></a>
-<a style="float:left;" class="text_ramka" href="trybi"><div class="ramka">Замена труб водоснабжения и канализации<img class="swf" src="/images/trybi.gif" width="80" height="61"></div></a>
-<a style="float:left;" class="text_ramka" href="zamena"><div class="ramka">Ремонт сантехники<img class="swf" src="/images/zamena.gif" width="51" height="70"></div></a>
-<a style="float:left;" class="text_ramka" href="polotencesyshitel"><div class="ramka">Установка полотенцесушителя<img class="swf" src="/images/polotence.gif" width="55" height="75"></div></a>
-<a style="float:left;" class="text_ramka" href="stiralka"><div class="ramka">Установка стиральной машины<p><img class="swf" src="/images/stiralka.gif" width="54" height="68"></p></div></a>
+<a style="float:left;" class="text_ramka" href="schetchiki"><div class="ramka_4">Счетчики воды. Установка и замена<img class="swf" src="/images/schetchiki.gif" width="80" height="40"></div></a>	
+<a style="float:left;" class="text_ramka" href="radiatory"><div class="ramka_4">Замена радиаторов отопления<img class="swf" src="/images/radiator.gif" width="55" height="40"></div></a>
+<a style="float:left;" class="text_ramka" href="trybi"><div class="ramka_4">Замена труб водоснабжения и канализации<img class="swf" src="/images/trybi.gif" width="60" height="40"></div></a>
+<a style="float:left;" class="text_ramka" href="zamena"><div class="ramka_4">Ремонт сантехники<p><img class="swf" src="/images/zamena.gif" width="40" height="40"></p></div></a>
+<a style="float:left;" class="text_ramka" href="polotencesyshitel"><div class="ramka_4">Установка полотенцесушителя<img class="swf" src="/images/polotence.gif" width="40" height="40"></div></a>
+<a style="float:left;" class="text_ramka" href="stiralka"><div class="ramka_4">Установка стиральной машины<p><img class="swf" src="/images/stiralka.gif" width="42" height="45"></p></div></a>
 
-<a class="text_ramka" href="pol"><div class="ramka">Установка теплого пола<img class="swf" src="/images/pol.gif" width="95" height="79"></div></a>
-<a class="text_ramka" href="smesitel"><div class="ramka">Установка смесителя<img class="swf" src="/images/smesitel.gif" width="90" height="60"></div></a>
-<a class="text_ramka" href="dyshevaia"><div class="ramka">Установка душевой кабины<img class="swf" src="/images/dyshevaia.gif" width="60" height="76"></div></a>
-<a class="text_ramka" href="vodonagrevatel"><div class="ramka">Установка водонагревателя<img class="swf" src="/images/vodonagrevatel.gif" width="40" height="73"></div></a>
-<a class="text_ramka" href="ynitaz"><div class="ramka">Установка унитаза<img class="swf" src="/images/ynitaz.gif" width="65" height="75"></div></a>
-<a class="text_ramka" href="vanna"><div class="ramka">Установка ванны, умывальника<img class="swf" src="/images/vanna.gif" width="80" height="65"></div></a>
+<a style="float:left;" class="text_ramka" href="pol"><div class="ramka_4">Установка теплого пола<img class="swf" src="/images/pol.gif" width="60" height="50"></div></a>
+<a style="float:left;" class="text_ramka" href="smesitel"><div class="ramka_4">Установка смесителя<img class="swf" src="/images/smesitel.gif" width="60" height="40"></div></a>
+<a style="float:left;" class="text_ramka" href="dyshevaia"><div class="ramka_4">Установка душевой кабины<img class="swf" src="/images/dyshevaia.gif" width="45" height="40"></div></a>
+<a style="float:left;" class="text_ramka" href="vodonagrevatel"><div class="ramka_4">Установка водонагревателя<img class="swf" src="/images/vodonagrevatel.gif" width="35" height="40"></div></a>
+<a style="float:left;" class="text_ramka" href="ynitaz"><div class="ramka_4">Установка унитаза<p><img class="swf" src="/images/ynitaz.gif" width="45" height="40"></p></div></a>
+<a style="float:left;" class="text_ramka" href="vanna"><div class="ramka_4">Установка ванны, умывальника<img class="swf" src="/images/vanna.gif" width="60" height="40	"></div></a>
 </div>
 
 <div class="ramka_3">
