1736b6f482241a7b248e2b4feae5827771c6c589
kostya mazur <kostya mazur>
2016-04-19 23:53:53 +0300

    update

--- application/views/price/p_6.php
+++ application/views/price/p_6.php
@@ -1,6 +1,6 @@
 <div class="m"><h2 class="house-header">СТОИМОСТЬ УСТАНОВКИ СМЕСИТЕЛЯ</h2><hr size="2" color="orange"><br>
 <div class="castom">
-<table class="tab" style="text-align: left; width: 100%;height:270px" border="1"
+<table class="tab" style="text-align: left; width: 100%;height:300px" border="1"
  cellpadding="0" cellspacing="0">
   <tbody>
     <tr>
